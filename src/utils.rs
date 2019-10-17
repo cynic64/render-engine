41c479408d810cc422a6a2546ddd10116b369ad4
cynic64 <f3bl@gmx.com>
2019-10-17 16:14:45 +0200

    bufferize_slice takes a usage now

--- src/utils.rs
+++ src/utils.rs
@@ -4,9 +4,9 @@ use vulkano::memory::Content;
 
 use std::sync::Arc;
 
-pub fn bufferize_slice<T: Content + 'static + Send + Sync + Clone>(queue: Arc<Queue>, slice: &[T]) -> Arc<ImmutableBuffer<[T]>>
+pub fn bufferize_slice<T: Content + 'static + Send + Sync + Clone>(queue: Arc<Queue>, slice: &[T], usage: BufferUsage) -> Arc<ImmutableBuffer<[T]>>
 {
-    ImmutableBuffer::from_iter(slice.iter().cloned(), BufferUsage::all(), queue).unwrap().0
+    ImmutableBuffer::from_iter(slice.iter().cloned(), usage, queue).unwrap().0
 }
 
 pub fn bufferize_data<T: Content + 'static + Send + Sync>(queue: Arc<Queue>, data: T) -> Arc<ImmutableBuffer<T>>
