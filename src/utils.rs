6c1b75a71bcbbedc0f8237ab5bd5f75a76262c8a
cynic64 <f3bl@gmx.com>
2019-10-18 18:23:15 +0200

    added load_texture

--- src/utils.rs
+++ src/utils.rs
@@ -1,23 +1,73 @@
-use vulkano::buffer::{ImmutableBuffer, BufferUsage};
-use vulkano::device::Queue;
-use vulkano::memory::Content;
+use vulkano::buffer::{BufferUsage, ImmutableBuffer};
 use vulkano::descriptor::DescriptorSet;
+use vulkano::device::Queue;
+use vulkano::format::Format;
+use vulkano::image::{Dimensions, ImageViewAccess, ImmutableImage};
+use vulkano::memory::Content;
 use vulkano::pipeline::input_assembly::PrimitiveTopology;
+use vulkano::sync::GpuFuture;
 
-use crate::system::{RenderableObject, Vertex};
+use image::ImageFormat;
+
 use crate::pipeline_cache::PipelineSpec;
+use crate::system::{RenderableObject, Vertex};
 
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
-use std::path::PathBuf;
 
-pub fn bufferize_slice<T: Content + 'static + Send + Sync + Clone>(queue: Arc<Queue>, slice: &[T]) -> Arc<ImmutableBuffer<[T]>>
-{
-    ImmutableBuffer::from_iter(slice.iter().cloned(), BufferUsage::all(), queue).unwrap().0
+pub fn bufferize_slice<T: Content + 'static + Send + Sync + Clone>(
+    queue: Arc<Queue>,
+    slice: &[T],
+) -> Arc<ImmutableBuffer<[T]>> {
+    ImmutableBuffer::from_iter(slice.iter().cloned(), BufferUsage::all(), queue)
+        .unwrap()
+        .0
 }
 
-pub fn bufferize_data<T: Content + 'static + Send + Sync>(queue: Arc<Queue>, data: T) -> Arc<ImmutableBuffer<T>>
-{
-    ImmutableBuffer::from_data(data, BufferUsage::all(), queue).unwrap().0
+pub fn bufferize_data<T: Content + 'static + Send + Sync>(
+    queue: Arc<Queue>,
+    data: T,
+) -> Arc<ImmutableBuffer<T>> {
+    ImmutableBuffer::from_data(data, BufferUsage::all(), queue)
+        .unwrap()
+        .0
+}
+
+pub fn load_texture(queue: Arc<Queue>, path: &Path) -> Arc<dyn ImageViewAccess + Send + Sync> {
+    let (texture, tex_future) = {
+        let image = image::load_from_memory_with_format(
+            &File::open(path)
+                .unwrap()
+                .bytes()
+                .map(|x| x.unwrap())
+                .collect::<Vec<u8>>(),
+            ImageFormat::PNG,
+        )
+        .unwrap()
+        .to_rgba();
+        let image_data = image.into_raw().clone();
+
+        ImmutableImage::from_iter(
+            image_data.iter().cloned(),
+            Dimensions::Dim2d {
+                width: 1024,
+                height: 1024,
+            },
+            Format::R8G8B8A8Srgb,
+            queue.clone(),
+        )
+        .unwrap()
+    };
+
+    tex_future
+        .then_signal_fence_and_flush()
+        .unwrap()
+        .wait(None)
+        .unwrap();
+
+    texture
 }
 
 // TODO: move this to its own file, bufferize too
