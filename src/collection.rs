41fe2f15bf8d39784d70e73285a0617b98e0df5f
cynic64 <f3bl@gmx.com>
2019-11-03 15:40:48 +0100

    improved collections

--- src/collection.rs
+++ src/collection.rs
@@ -19,7 +19,9 @@ pipeline and queue.
  */
 use vulkano::device::Queue;
 use vulkano::pipeline::GraphicsPipelineAbstract;
-use vulkano::descriptor::DescriptorSet;
+use vulkano::descriptor::descriptor_set::{DescriptorSet, PersistentDescriptorSet};
+
+use crate::utils::bufferize_data;
 
 use std::sync::Arc;
 
@@ -40,3 +42,83 @@ impl Collection for () {
         vec![]
     }
 }
+
+impl<T: Set> Collection for (T,) {
+    fn convert(
+        &self,
+        queue: Arc<Queue>,
+        pipeline: Arc<dyn GraphicsPipelineAbstract + Send + Sync>,
+    ) -> Vec<Arc<dyn DescriptorSet + Send + Sync>> {
+        vec![
+            self.0.upload(queue, pipeline)
+        ]
+    }
+}
+
+impl<T1: Set, T2: Set> Collection for (T1, T2) {
+    fn convert(
+        &self,
+        queue: Arc<Queue>,
+        pipeline: Arc<dyn GraphicsPipelineAbstract + Send + Sync>,
+    ) -> Vec<Arc<dyn DescriptorSet + Send + Sync>> {
+        vec![
+            self.0.upload(queue.clone(), pipeline.clone()),
+            self.1.upload(queue.clone(), pipeline.clone()),
+        ]
+    }
+}
+
+/*
+Set
+ */
+
+pub trait Set {
+    fn upload(
+        &self,
+        queue: Arc<Queue>,
+        pipeline: Arc<dyn GraphicsPipelineAbstract + Send + Sync>,
+    ) -> Arc<dyn DescriptorSet + Send + Sync>;
+}
+
+impl<T: Data> Set for (T,) {
+    fn upload(
+        &self,
+        queue: Arc<Queue>,
+        pipeline: Arc<dyn GraphicsPipelineAbstract + Send + Sync>,
+    ) -> Arc<dyn DescriptorSet + Send + Sync> {
+        let buffer = bufferize_data(queue.clone(), self.0.clone());
+
+        Arc::new(
+            PersistentDescriptorSet::start(pipeline, 0)
+                .add_buffer(buffer)
+                .unwrap()
+                .build()
+                .unwrap()
+        )
+    }
+}
+
+impl<T1: Data, T2: Data> Set for (T1, T2) {
+    fn upload(
+        &self,
+        queue: Arc<Queue>,
+        pipeline: Arc<dyn GraphicsPipelineAbstract + Send + Sync>,
+    ) -> Arc<dyn DescriptorSet + Send + Sync> {
+        let buffer1 = bufferize_data(queue.clone(), self.0.clone());
+        let buffer2 = bufferize_data(queue.clone(), self.1.clone());
+
+        Arc::new(
+            PersistentDescriptorSet::start(pipeline, 0)
+                .add_buffer(buffer1)
+                .unwrap()
+                .add_buffer(buffer2)
+                .unwrap()
+                .build()
+                .unwrap()
+        )
+    }
+}
+
+pub trait Data: Send + Sync + Clone + 'static {}
+
+impl<T: Send + Sync + Clone + 'static> Data for T {}
